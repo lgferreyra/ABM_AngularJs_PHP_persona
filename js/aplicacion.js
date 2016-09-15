7f6849625e93126ab808aa1a9a383c1f649f327b
lgferreyra <leonardogferreyra@gmail.com>
2016-09-15 10:44:40 -0300

    asdffasfdsfa

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp",['ui.router']);
+var app = angular.module("myApp",['ui.router', 'angularFileUpload']);
 
 app.config( function($stateProvider, $urlRouterProvider){
 
@@ -230,7 +230,67 @@ app.controller("controlPersonaMenu", function($scope, $state){
   };
 });
 
-app.controller("controlPersonaAlta", function($scope){
+app.controller("controlPersonaAlta", function($scope, FileUploader){
+
+  $scope.uploader = new FileUploader({url: 'PHP/upload.php'});
+  $scope.uploader.queueLimit = 1; // indico cuantos archivos permito cargar
+            
+      /* Si quiero restringir los archivos a imagenes añado este filtro */
+      $scope.uploader.filters.push({
+              name: 'imageFilter',
+              fn: function(item, options) {
+                  var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                  return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+              }
+          });
+
+          /** funcion para mi boton cargar si quiero agregar funcionalidad, 
+           de lo contrario uso el item.upload() en la vista **/
+          $scope.cargar = function(){
+            /** llamo a la funcion uploadAll para cargar toda la cola de archivos **/
+            $scope.uploader.uploadAll();
+            /** agrego mi funcionalidad **/
+
+          }
+      /***********************************************
+      *  Funciones callbacks que nos dan informacion *
+      *  en el proceso de carga de archivos          *
+      ***********************************************/
+
+      // $scope.uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+      //     console.info('onWhenAddingFileFailed', item, filter, options);
+      // };
+    //       $scope.uploader.onAfterAddingFile = function(fileItem) {
+    //           console.info('onAfterAddingFile', fileItem);
+    //       };
+    //       $scope.uploader.onAfterAddingAll = function(addedFileItems) {
+    //           console.info('onAfterAddingAll', addedFileItems);
+    //       };
+    //       $scope.uploader.onBeforeUploadItem = function(item) {
+    //           console.info('onBeforeUploadItem', item);
+    //       };
+    //       $scope.uploader.onProgressItem = function(fileItem, progress) {
+    //           console.info('onProgressItem', fileItem, progress);
+    //       };
+    //       $scope.uploader.onProgressAll = function(progress) {
+    //           console.info('onProgressAll', progress);
+    //       };
+    //       $scope.uploader.onSuccessItem = function(fileItem, response, status, headers) {
+    //           console.info('onSuccessItem', fileItem, response, status, headers);
+    //       };
+          $scope.uploader.onErrorItem = function(fileItem, response, status, headers) {
+              console.info('onErrorItem', fileItem, response, status, headers);
+          };
+    //       $scope.uploader.onCancelItem = function(fileItem, response, status, headers) {
+    //           console.info('onCancelItem', fileItem, response, status, headers);
+    //       };
+    //       $scope.uploader.onCompleteItem = function(fileItem, response, status, headers) {
+    //           console.info('onCompleteItem', fileItem, response, status, headers);
+    //       };
+          $scope.uploader.onCompleteAll = function() {
+              console.info('Se cargo con exito');
+          };
+
 
 });
 
